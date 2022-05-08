bab5f72d3f21871f50110afa0bf8d4575d5b57b6
Joey Huab <joey@evolution-x.org>
2022-05-08 15:20:29 +0900

    spes: Use TARGET_USES_MINI_GAPPS flag

--- evolution_spes.mk
+++ evolution_spes.mk
@@ -15,6 +15,7 @@ $(call inherit-product, device/xiaomi/spes/device.mk)
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_MINI_GAPPS := true
 
 TARGET_USES_BLUR := true
 USE_PIXEL_CHARGING := true
