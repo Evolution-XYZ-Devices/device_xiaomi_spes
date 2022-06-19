e6c42eec9c43a07cc298a27a3f8bdb4d944b1cce
etylix <me@etylix.me>
2022-06-19 16:39:16 +0700

    spes: RemovePackages: update

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,19 +4,41 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService arcore
-LOCAL_OVERRIDES_PACKAGES += CarrierSetup Chrome Chrome-Stub ConnMO ConnMetrics
-LOCAL_OVERRIDES_PACKAGES += DCMO DevicePolicyPrebuilt DMService Drive 
-LOCAL_OVERRIDES_PACKAGES += GCS GoogleCamera
-LOCAL_OVERRIDES_PACKAGES += MaestroPrebuilt Maps MyVerizonServices
-LOCAL_OVERRIDES_PACKAGES += PixelLiveWallpaperPrebuilt Photos PrebuiltGmail
-LOCAL_OVERRIDES_PACKAGES += RecorderPrebuilt
-LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SCONE ScribePrebuilt Showcase SoundAmplifierPrebuilt SprintDM SprintHM
-LOCAL_OVERRIDES_PACKAGES += Tycho
-LOCAL_OVERRIDES_PACKAGES += USCCDM
-LOCAL_OVERRIDES_PACKAGES += Videos VZWAPNLib VzwOmaTrigger
-LOCAL_OVERRIDES_PACKAGES += WallpapersBReel2020
-LOCAL_OVERRIDES_PACKAGES += YouTube YouTubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES := \
+    arcore \
+    Camera2 \
+    ConnMO \
+    ConnMetrics \
+    DCMO \
+    DevicePolicyPrebuilt \
+    DMService \
+    Drive \
+    GCS \
+    GoogleCamera \
+    Maps \
+    Music \
+    MyVerizonServices \
+    OBDM_Permissions \
+    obdm_stub \
+    OdadPrebuilt \
+    OemDmTrigger \
+    OPScreenRecord \
+    Ornament \
+    PlayAutoInstallConfig \
+    SCONE \
+    ScribePrebuilt \
+    Showcase \
+    Snap \
+    Snap2 \
+    SnapdragonCamera \
+    SoundAmplifierPrebuilt \
+    SprintDM \
+    SprintHM \
+    Tycho \
+    USCCDM \
+    Videos \
+    YouTube \
+    YouTubeMusicPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
