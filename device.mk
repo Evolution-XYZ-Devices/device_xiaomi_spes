35be439f28be956338f483be0a556138f69545a2
etylix <me@etylix.me>
2022-06-21 21:09:58 +0700

    Revert "spes: rootdir: Switch to stock USB configs"
    This reverts commit 5a7f08c3b1f0ae9e438ba609ea6ed62f67fb41df.

--- device.mk
+++ device.mk
@@ -421,17 +421,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.mdm.sh \
     init.qcom.post_boot.sh \
-    init.qcom.sh \
-    init.qcom.sh \
-    init.qcom.usb.sh \
-    init.qti.dcvs.sh
+    init.qcom.sh
 
 PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.qcom_ramdisk \
     init.qcom.power.rc \
     init.qcom.rc \
-    init.qcom.usb.rc \
     init.recovery.qcom.rc \
     init.spes.perf.rc \
     init.stnfc.rc \
@@ -473,6 +469,13 @@ PRODUCT_PACKAGES_DEBUG += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
+
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
