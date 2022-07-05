f0794ac0f94ca84e2eb2e7d43f79d1a7b69bcdab
Hoang Pham Anh Duy <duy@etylix.me>
2022-07-05 14:14:57 +0700

    spes: move to GCam GO

--- device.mk
+++ device.mk
@@ -258,6 +258,9 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
+# GCam Go
+PRODUCT_PACKAGES += \
+    GCamGOPrebuilt-V3
 # GNSS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-service-qti
