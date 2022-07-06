415618d173b729d704155d8d75ca230e37533b5b
Hoang Pham Anh Duy <duy@etylix.me>
2022-07-06 17:37:32 +0700

    spes: libinit: remove market name

--- libinit/init_xiaomi_spes.cpp
+++ libinit/init_xiaomi_spes.cpp
@@ -15,7 +15,6 @@ static const variant_info_t spes_info = {
 
     .brand = "Redmi",
     .device = "spes",
-    .marketname = "Redmi Note 11",
     .model = "2201117TG",
     .build_fingerprint = "Redmi/spes_global/spes:11/RKQ1.211001.001/V13.0.9.0.RGCMIXM:user/release-keys",
 
@@ -28,7 +27,6 @@ static const variant_info_t spes_in_info = {
 
     .brand = "Redmi",
     .device = "spes",
-    .marketname = "Redmi Note 11",
     .model = "2201117TI",
     .build_fingerprint = "Redmi/spes_in/spes:11/RKQ1.211001.001/V13.0.8.0.RGCINXM:user/release-keys",
 
@@ -41,7 +39,6 @@ static const variant_info_t spesn_info = {
 
     .brand = "Redmi",
     .device = "spesn",
-    .marketname = "Redmi Note 11",
     .model = "2201117TY",
     .build_fingerprint = "Redmi/spesn_global/spesn:11/RKQ1.211001.001/V13.0.9.0.RGCMIXM:user/release-keys",
 
