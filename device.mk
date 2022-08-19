0ac8e9a59945c03450856ee3f11afd4ec910517e
Lucchetto <lucchetto.tie@live.com>
2022-08-19 10:48:18 +0200

    spes: Copy prebuilt libhidlbase from vndk v32
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -523,6 +523,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_EXTRA_VNDK_VERSIONS := 30
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
