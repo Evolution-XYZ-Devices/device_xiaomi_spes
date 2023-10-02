3f80b73bece618bb9e69f5fbeb08ce14504e6a42
Hoang Pham Anh Duy <duy@etylix.me>
2023-10-02 18:11:43 +0700

    spes: libperfmgr: Rebrand for spes
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- power-libperfmgr/PowerExt.cpp
+++ power-libperfmgr/PowerExt.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power-service.pixel.ext-libperfmgr"
+#define LOG_TAG "android.hardware.power-service.spes.ext-libperfmgr"
 
 #include "PowerExt.h"
 
