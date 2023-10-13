ee19fa3d48cebc3bdb213bc4c0ca2b9624124a50
Hoang Pham Anh Duy <duy@etylix.me>
2023-10-13 14:45:20 +0700

    Revert "spes: libinit: Disable OEM Unlock"
    This reverts commit 36cef588ba705da191ebb381d2dbe9829321360c.

--- libinit/init_xiaomi_spes.cpp
+++ libinit/init_xiaomi_spes.cpp
@@ -61,6 +61,4 @@ void vendor_load_properties() {
 
     // SafetyNet workaround
     property_override("ro.boot.verifiedbootstate", "green");
-    property_override("ro.oem_unlock_supported", "0");
-
 }
