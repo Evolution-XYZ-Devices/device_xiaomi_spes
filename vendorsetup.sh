381fbcc373e7fb5769cf8255f1e468353c58fcee
Hoang Pham Anh Duy <duy@etylix.me>
2023-11-14 21:32:00 +0700

    spes: Update dependencies

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,11 @@
-echo 'Cloning sepolicy_vndr-legacy-um'
+echo 'Cloning sepolicy_vndr-legacy-um...'
 rm -rf device/qcom/sepolicy_vndr-legacy-um
-git clone --depth=1 https://github.com/etylix/device_qcom_sepolicy_vndr-legacy-um device/qcom/sepolicy_vndr-legacy-um
+git clone -q --depth=1 https://github.com/etylix/device_qcom_sepolicy_vndr-legacy-um device/qcom/sepolicy_vndr-legacy-um
 
-echo 'Cloning clang-r450784e'
-git clone https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone --depth=1 -b 14 prebuilts/clang/host/linux-x86/clang-r450784e
+echo 'Cloning clang-r450784e...'
+git clone -q https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone --depth=1 -b 14 prebuilts/clang/host/linux-x86/clang-r450784e
 
-echo 'Removing Pixel kernel headers'
+echo 'Removing Pixel kernel headers...'
 rm -rf hardware/google/pixel/kernel_headers/
+
+echo 'DONE'
